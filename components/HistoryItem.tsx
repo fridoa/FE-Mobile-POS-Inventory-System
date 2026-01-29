fee2aeab28fb00d1d4865d094f18b4007c2aae99
fridoa <fridoafriyanto3@gmail.com>
2026-01-29 21:58:51 +0700

    feat(history): enhance date display logic and improve code readability

--- components/HistoryItem.tsx
+++ components/HistoryItem.tsx
@@ -1,7 +1,9 @@
 import { ITransactionHistoryItem } from "@/services/transaction.service";
 import formatRupiah from "@/utils/formatRupiah";
+import { format, isToday } from "date-fns";
+import { id } from "date-fns/locale";
 import { ChevronRight, ReceiptText } from "lucide-react-native";
-import React, { memo } from "react";
+import React, { memo, useMemo } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 interface HistoryItemProps {
@@ -10,25 +12,27 @@ interface HistoryItemProps {
 }
 
 const HistoryItem = memo(({ item, onPress }: HistoryItemProps) => {
-  const time = new Date(item.createdAt).toLocaleTimeString("id-ID", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  const displayDate = useMemo(() => {
+    const date = new Date(item.createdAt);
+
+    if (isToday(date)) {
+      return `Hari ini, ${format(date, "HH:mm")}`;
+    }
+    return format(date, "dd MMM, HH:mm", { locale: id });
+  }, [item.createdAt]);
 
   const totalItems = item.items.reduce((acc, curr) => acc + curr.quantity, 0);
 
   return (
     <TouchableOpacity onPress={onPress} activeOpacity={0.7} className="flex-row items-center p-4 mb-3 bg-white border shadow-sm border-slate-100 rounded-2xl">
-      {/* Icon Area */}
       <View className="items-center justify-center w-12 h-12 rounded-xl bg-emerald-50">
         <ReceiptText size={22} color="#059669" />
       </View>
 
-      {/* Info Area */}
       <View className="flex-1 ml-4">
         <View className="flex-row items-center justify-between">
-          <Text className="text-xs font-bold tracking-tighter uppercase text-slate-400">{item.transactionNumber}</Text>
-          <Text className="text-[10px] text-slate-400 font-medium">{time}</Text>
+          <Text className="text-[10px] font-bold tracking-tighter uppercase text-slate-400">{item.transactionNumber}</Text>
+          <Text className="text-[10px] text-slate-400 font-bold">{displayDate}</Text>
         </View>
 
         <Text className="mt-0.5 text-base font-black text-slate-800">{formatRupiah(item.totalAmount)}</Text>
@@ -36,7 +40,6 @@ const HistoryItem = memo(({ item, onPress }: HistoryItemProps) => {
         <Text className="text-[10px] text-slate-400 mt-1 font-medium">{totalItems} Produk • Tunai</Text>
       </View>
 
-      {/* Action Area */}
       <View className="ml-2">
         <ChevronRight size={18} color="#CBD5E1" />
       </View>
@@ -44,8 +47,5 @@ const HistoryItem = memo(({ item, onPress }: HistoryItemProps) => {
   );
 });
 
-
-
 HistoryItem.displayName = "HistoryItem";
-
 export default HistoryItem;
